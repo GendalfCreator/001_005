2f9f1e3a93410a58cb3a68466f5e1e237acc87cc
Alex <frstfng@gmail.com>
2021-02-13 21:15:57 +0300

    2-nd quest complete

--- main.cpp
+++ main.cpp
@@ -3,24 +3,46 @@
 using namespace std;
 
 int Inversion(int* array) {
-    for (int i = 0; i < 10; i++) {
+    for(int i = 0; i < 10; i++) {
         if (*(array + i) == 0) {
             *(array + i) = 1;
         }
         else {
             *(array + i) = 0;
         }
-    cout << *(array + i) << (i < 9 ? "," : ".") << " ";
     }
     return 0;
 }
 
+int Progression(int* array) {
+    *array = 1;
+    for(int i = 1; i < 8; i++) {
+        *(array + i) = *(array + i - 1) + 3;
+    }
+    return 0;
+}
+
+void PrintArray(int* array, int n) {
+    for(int i = 0; i < n; i++) {
+        cout << *(array + i) << " ";
+    }
+}
 
 int main()
 {
-    int ExampleArray_0[10] = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
+    int example_array_0[10] = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
 
-    Inversion(ExampleArray_0);
+    PrintArray(example_array_0, 10);
+    cout << endl;
+
+    Inversion(example_array_0);
+    PrintArray(example_array_0, 10);
+    cout << endl;
+
+    int example_array_1[8] = {};
+
+    Progression(example_array_1);
+    PrintArray(example_array_1, 8);
     cout << endl;
 
     return 0;
