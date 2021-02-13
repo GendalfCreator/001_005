d17c0a5f6497300ee24f2c13c936f36f31b802ce
Alex <frstfng@gmail.com>
2021-02-13 20:44:26 +0300

    1-st quest comlete

--- main.cpp
+++ main.cpp
@@ -1,9 +1,27 @@
 #include <iostream>
-
+#include <string>
 using namespace std;
+
+int Inversion(int* array) {
+    for (int i = 0; i < 10; i++) {
+        if (*(array + i) == 0) {
+            *(array + i) = 1;
+        }
+        else {
+            *(array + i) = 0;
+        }
+    cout << *(array + i) << (i < 9 ? "," : ".") << " ";
+    }
+    return 0;
+}
+
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    int ExampleArray_0[10] = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
+
+    Inversion(ExampleArray_0);
+    cout << endl;
+
     return 0;
 }
